3fb7916c89ed1056d786362f82df3e08bdbaf209
Aaron Evans <aarone@outlook.com.au>
2024-09-18 22:01:59 +1000

    Add outputs for TCP and SSH target IDs with debug output for alias destination ID logic

---
+++ outputs.tf
@@ -0,0 +1,19 @@
+# Output for TCP with credentials target IDs
+output "tcp_with_creds_target_ids" {
+  value = { for key, target in boundary_target.tcp_with_creds : key => target.id }
+}
+
+# Output for SSH with credentials target IDs
+output "ssh_with_creds_target_ids" {
+  value = { for key, target in boundary_target.ssh_with_creds : key => target.id }
+}
+
+
+# Debug output to check alias destination ID logic
+output "alias_destination_ids" {
+  value = {
+    for host_key, host in boundary_host_static.this : host_key => {
+      destination_id = contains(keys(boundary_target.tcp_with_creds), host_key) ? boundary_target.tcp_with_creds[host_key].id : contains(keys(boundary_target.ssh_with_creds), host_key) ? boundary_target.ssh_with_creds[host_key].id : null
+    }
+  }
+}
