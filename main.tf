f47c6747d51a142678cead7c0aa4eb62e5d89c44
Aaron Evans <aarone@outlook.com.au>
2024-10-30 21:59:42 +1100

    Refactor credential injection logic for SSH targets and improve readability

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # Local values for credential configuration based on inputs
 locals {
-  use_vault_creds = var.credential_source == "vault" && var.use_credentials
-  credential_store_id = var.existing_credential_store_id != null ? var.existing_credential_store_id :  (length(boundary_credential_store_vault.this) > 0 ? boundary_credential_store_vault.this[0].id : null)
+  use_vault_creds     = var.credential_source == "vault" && var.use_credentials
+  credential_store_id = var.existing_credential_store_id != null ? var.existing_credential_store_id : (length(boundary_credential_store_vault.this) > 0 ? boundary_credential_store_vault.this[0].id : null)
 }
 
 # Data Sources to get the organizational and project scopes
@@ -26,7 +26,7 @@ resource "boundary_host_catalog_static" "this" {
 resource "boundary_host_static" "this" {
   for_each        = toset(var.hosts)
   type            = "static"
-  name            = "${each.key}"
+  name            = each.key
   host_catalog_id = boundary_host_catalog_static.this.id
   address         = each.key
 }
@@ -83,10 +83,30 @@ resource "boundary_target" "this" {
   scope_id        = data.boundary_scope.project.id
   host_source_ids = [boundary_host_set_static.this.id] # References the host set, even if it's one host
 
-  # Conditional injection based on credential source
+  # Define credential source IDs for SSH, conditional on target mode and credential source
   injected_application_credential_source_ids = (
-    local.use_vault_creds && var.target_type == "ssh" && var.target_mode == "single"
-  ) ? compact([lookup(boundary_credential_library_vault_ssh_certificate.ssh, var.hosts[0], "").id]) : null
+    # SINGLE TARGET MODE LOGIC
+    var.target_type == "ssh" && var.target_mode == "single"
+    ) ? (
+    # If using Vault-managed credentials in single mode
+    local.use_vault_creds ?
+    # Attempt to look up Vault-managed SSH credential library for the first host
+    compact([lookup(boundary_credential_library_vault_ssh_certificate.ssh, var.hosts[0], "").id]) :
+    # Else, use existing SSH credentials for the first host if Module creation is not being used
+    compact([lookup(var.existing_ssh_credential_libraries, var.hosts[0], null)])
+    ) : (
+    # GROUP TARGET MODE LOGIC
+    var.target_type == "ssh" && var.target_mode == "group" ?
+    # Iterate over each host in group mode to inject SSH credentials
+    compact([for host in var.hosts : local.use_vault_creds ?
+      # Use Vault-managed SSH credentials for each host in the group if Vault credentials are enabled
+      lookup(boundary_credential_library_vault_ssh_certificate.ssh, host, null) :
+      # Otherwise, use existing SSH credentials if Module credentials are not being used
+      lookup(var.existing_ssh_credential_libraries, host, null)
+      # Fallback to null if neither single nor group mode is specified for SSH
+    ]) : null
+  )
+
 
   brokered_credential_source_ids = (
     local.use_vault_creds && var.target_type == "tcp" && var.target_mode == "single"
@@ -99,9 +119,9 @@ resource "boundary_target" "this" {
 
 # Boundary Target Alias
 resource "boundary_alias_target" "alias" {
-  name                     = var.alias_name != "" ? var.alias_name : "${var.target_name}"
-  value                    = var.alias_name != "" ? var.alias_name : "${var.target_name}"
-  scope_id                 = "global"
-  destination_id           = boundary_target.this.id
+  name                      = var.alias_name != "" ? var.alias_name : "${var.target_name}"
+  value                     = var.alias_name != "" ? var.alias_name : "${var.target_name}"
+  scope_id                  = "global"
+  destination_id            = boundary_target.this.id
   authorize_session_host_id = element([for host in boundary_host_static.this : host.id], 0)
 }
